b55a792e10240777db9ddfa54780fea30fd42e21
Johnberman-J <86252835+Johnberman-J@users.noreply.github.com>
2021-10-15 16:08:18 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(h2Title, list, body) {
   return  `
             <!doctype html>
             <html>
             <head>
-              <title>WEB1 - ${title}</title>
+              <title>WEB1 - ${h2Title}</title>
               <meta charset="utf-8">
             </head>
             <body>
               <h1><a href="/">WEB</a></h1>
               ${list}
+              <a href="/create">Create</a>
               ${body}
             </body>
             </html>
@@ -37,6 +39,7 @@ var app = http.createServer(function(request,response){
     var pathName = url.parse(_url, true).pathname;
     var title = queryData.id;
 
+
     if(pathName === '/') {
       if(queryData.id === undefined) {
 
@@ -44,7 +47,7 @@ var app = http.createServer(function(request,response){
           var title = `Johnberman's Blog`;
           var description = 'Hello, Node.JS!';
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+          var template = templateHTML(title, list, `<h2>${title.substring(0,(title.length-4))}</h2><p>${description}</p>`);
 
           response.writeHead(200);
           response.end(template);
@@ -54,14 +57,46 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(err, filelist) {
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+            var template = templateHTML(title, list, `<h2>${title.substring(0,(title.length-4))}</h2><p>${description}</p>`);
+
             response.writeHead(200);
             response.end(template);
           });
       });
     }
 
-    } else {
+  } else if(pathName === '/create') {
+
+      fs.readdir('./data', function(err, filelist) {
+        var title = `WEB - Create`;
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+            <form action="http://localhost:3000/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p><textarea name="description" placeholder="description"></textarea></p>
+              <p><input type="submit"></p>
+            </form>
+        `);
+
+        response.writeHead(200);
+        response.end(template);
+      })
+
+  } else if(pathName === '/create_process') {
+      var body = '';
+      request.on('data', function(data) {
+        body = body + data;
+        // 아래는 패킷이 너무 클경우 연결을 끊어버리는 일종의 보안장치.
+        // if (body.length > 1e6)
+        //   request.connection.destroy();
+      });
+      request.on('end', function() {
+        var post = qs.parse(body);
+      });
+
+      response.writeHead(404);
+      response.end('Success');
+  } else {
       response.writeHead(404);
       response.end('Not found');
     }
